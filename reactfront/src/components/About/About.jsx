5aac771b57d9306e911b99beeb82339046ae2801
danilovictorio <a17danvicfer@inspedralbes.cat>
2024-09-20 19:06:06 +0200

    cambios about y empezando services

--- reactfront/src/components/About/About.jsx
+++ reactfront/src/components/About/About.jsx
@@ -3,19 +3,28 @@ import Style from './About.module.css';
 
 const About = () => {
   return (
-    <div className={Style.About}>
+    <div className={Style.About} id='about'>
       <div className={Style.Description}>
-      <h1 className={Style.Title}>Puertas BlockLemon</h1>
+      <h1 className={Style.Title}>PUERTAS BLOCKLEMON</h1>
       <h3 className={Style.Subtitle}>Diseño y fabricacion de puertas</h3>
       <p className={Style.Text}>En BlockLemon somos especialistas en diseñar y fabricar puertas en Barcelona, tanto de interior como de exterior. Fabricamos todo tipos de puertas, es por ello que somos especialistas en el tratado de madera.
       Realizamos proyectos de puertas a medida, pudiendo elegir entre puertas de interior blancas o de madera, puertas de cocina, puertas de entrada y/o armarios.</p>
-      <button>Más Info</button>
+      <button className={Style.Button}>Más Info</button>
       </div>
       <div className={Style.Resumen}>
-      
+      <div className={Style.Card}>
+        <h1>40+</h1>
+      <h2>Experencia</h2>
       </div>
-
-
+      <div className={Style.Card}>
+        <h1>4000+</h1>
+      <h2>Instalacio de puertas</h2>
+      </div>
+      <div className={Style.Card}>
+        <h1>4+</h1>
+      <h2>Valoracion Clientes</h2>
+      </div>
+      </div>
     </div>
   )
 }
--- reactfront/src/components/Services/Services.jsx
+++ reactfront/src/components/Services/Services.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Styles from './Services.module.css'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className={Styles.Services} id='services'>
+      <div className={Styles.Image}>
+      <img src="/puertas/puertaMedida.jpg" alt="" />
+      </div>
+
+      <div className={Styles.Description}>
+        <h1></h1>
+
+      </div>
+    </div>
   )
 }
 
